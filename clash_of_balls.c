5fc8b667326e28b8057c829a9616630e4e19aadf
Unaty <leon.muselli@gmail.com>
2024-11-15 15:17:41 +0100

    fix

--- clash_of_balls.c
+++ clash_of_balls.c
@@ -15,7 +15,7 @@
 const int BALL_SIZE = 300;
 const int SCREEN_WIDTH = 480;
 const int SCREEN_HEIGHT = 854;
-const int FRAME_RATE = 64;
+const int FRAME_RATE = 100;
 const float DT = 1000.0f / FRAME_RATE;
 
 typedef struct
@@ -29,18 +29,20 @@ typedef struct
     Vect speed;           /* Vitesse de la boule */
     Vect position;        /* Position de la boule */
     SDL_Texture *texture; /* Texture de la boule */
+    double mass;
+    int colliding;
 } Ball;
 
 /* Prototypes des fonctions */
 int initSDL();
-void createBall(Ball *ball, int size, Vect position, Vect speed, SDL_Texture *texture);
-void updateBall(Ball *ball, Ball *balls, int num_boule_actuelle, int nb_balls);
+void createBall(Ball *ball, int size, double mass, Vect position, Vect speed, SDL_Texture *texture);
+void updateBall(Uint64 dt, Ball *ball, Ball *balls, int num_boule_actuelle, int nb_balls);
 void drawBall(SDL_Renderer *renderer, Ball *ball);
 void cleanup(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *texture1, SDL_Texture *texture2, Ball *balls, int ballCount);
 void addVect(Vect v1, Vect v2, double coeff, Vect *dest);
 double dot(Vect v1, Vect v2);
 double norm(Vect v);
-void calculer_collision(Ball *b1, Ball *b2, double mA, double mB, Vect *vA_f, Vect *vB_f);
+void calculer_collision(Ball *b1, Ball *b2, Vect *vA_f, Vect *vB_f);
 
 int main(int argc, char *argv[])
 {
@@ -56,8 +58,10 @@ int main(int argc, char *argv[])
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     /* Charger l'image pour les boules */
-    SDL_Surface *imageSurfaceRouge = SDL_LoadBMP("boule_rouge.bmp");
-    SDL_Surface *imageSurfaceBleu = SDL_LoadBMP("boule_bleu.bmp");
+    SDL_Surface *imageSurfaceRouge = SDL_LoadBMP("./img/boule_rouge.bmp");
+    SDL_Surface *imageSurfaceBleu = SDL_LoadBMP("./img/boule_bleu.bmp");
+    SDL_Surface *imageSurfaceVerte = SDL_LoadBMP("./img/boule_verte.bmp");
+    SDL_Surface *imageSurfaceBleuClair = SDL_LoadBMP("./img/boule_bleu_clair.bmp");
     if (!imageSurfaceRouge)
     {
         printf("Erreur de chargement de l'image: %s\n", SDL_GetError());
@@ -70,25 +74,48 @@ int main(int argc, char *argv[])
         cleanup(window, renderer, NULL, NULL, NULL, 0);
         return -1;
     }
+    if (!imageSurfaceVerte)
+    {
+        printf("Erreur de chargement de l'image: %s\n", SDL_GetError());
+        cleanup(window, renderer, NULL, NULL, NULL, 0);
+        return -1;
+    }
+    if (!imageSurfaceBleuClair)
+    {
+        printf("Erreur de chargement de l'image: %s\n", SDL_GetError());
+        cleanup(window, renderer, NULL, NULL, NULL, 0);
+        return -1;
+    }
     SDL_Texture *imageTexture_bouleRouge = SDL_CreateTextureFromSurface(renderer, imageSurfaceRouge);
     SDL_Texture *imageTexture_bouleBleu = SDL_CreateTextureFromSurface(renderer, imageSurfaceBleu);
+    SDL_Texture *imageTexture_bouleVerte = SDL_CreateTextureFromSurface(renderer, imageSurfaceVerte);
+    SDL_Texture *imageTexture_bouleBleuClair = SDL_CreateTextureFromSurface(renderer, imageSurfaceBleuClair);
     SDL_FreeSurface(imageSurfaceRouge);
     SDL_FreeSurface(imageSurfaceBleu);
+    SDL_FreeSurface(imageSurfaceVerte);
+    SDL_FreeSurface(imageSurfaceBleuClair);
 
     /* Allocation et génération des boules */
-    int maxBalls = 2;
-    Ball *balls = (Ball *)malloc(maxBalls * sizeof(Ball));
-    Vect pos_depart_rouge = {100.0, 100.0};
-    Vect pos_depart_bleu = {200, 200};
-    Vect v_depart_rouge = {0.0, 0.0};
-    Vect v_depart_bleu = {6.0, 6.0};
-    createBall(&balls[0], 50, pos_depart_rouge, v_depart_bleu, imageTexture_bouleRouge);
-    createBall(&balls[1], 50, pos_depart_bleu, v_depart_rouge, imageTexture_bouleBleu);
+    int maxBalls = 4;
+    Ball *balls = malloc(maxBalls * sizeof(Ball));
+    if (!balls)
+    {
+        printf("Erreur d'allocation de mémoire\n");
+        cleanup(window, renderer, imageTexture_bouleRouge, imageTexture_bouleBleu, NULL, 0);
+        return -1;
+    }
+    createBall(&balls[0], 50, 10, (Vect) {100.0, 100.0}, (Vect) {600.0, 900.0}, imageTexture_bouleRouge);
+    createBall(&balls[1], 60, 20, (Vect) {300.0, 200.0}, (Vect) {0.0, 0.0}, imageTexture_bouleBleu);
+    createBall(&balls[2], 90, 100, (Vect) {400.0, 300.0}, (Vect) {0.0, 0.0}, imageTexture_bouleVerte);
+    createBall(&balls[3], 60, 20, (Vect) {300.0, 500.0}, (Vect) {0.0, 0.0}, imageTexture_bouleBleuClair);
 
     /* Boucle principale */
     bool isRunning = true;
     SDL_Event event;
-    Uint32 lastTime = SDL_GetTicks();
+    Uint64 lastTime = SDL_GetTicks();
+    Uint64 frameTime = SDL_GetTicks();
+    Uint64 elapsedTime = 0;
+    int frameCount = 0;
 
     while (isRunning)
     {
@@ -103,7 +130,7 @@ int main(int argc, char *argv[])
         /* Mise à jour des boules */
         for (int i = 0; i < maxBalls; i++)
         {
-            updateBall(&balls[i], balls, i, maxBalls);
+            updateBall(elapsedTime, &balls[i], balls, i, maxBalls);
         }
 
         /* Rafraîchissement de l'écran */
@@ -119,13 +146,26 @@ int main(int argc, char *argv[])
         SDL_RenderPresent(renderer);
 
         /* Contrôle du framerate */
-        Uint32 currentTime = SDL_GetTicks();
-        Uint32 elapsedTime = currentTime - lastTime;
-        if (elapsedTime < DT)
+        Uint64 currentTime = SDL_GetTicks64();
+        elapsedTime = currentTime - frameTime;
+        /*if (elapsedTime < DT)
         {
             SDL_Delay((int)(DT - elapsedTime));
+        }*/
+        frameCount++;
+
+        // Every second, display the FPS (frame count since the last second)
+
+        if (currentTime - lastTime >= 5000)
+        {
+            printf("FPS: %d\n", frameCount / 5);
+
+        
+            frameCount = 0;
+            lastTime = currentTime;
         }
-        lastTime = SDL_GetTicks();
+
+        frameTime = SDL_GetTicks64();
     }
 
     /* Libération des ressources */
@@ -144,7 +184,7 @@ int initSDL()
     return 0;
 }
 
-void createBall(Ball *ball, int size, Vect position, Vect speed, SDL_Texture *texture)
+void createBall(Ball *ball, int size, double mass, Vect position, Vect speed, SDL_Texture *texture)
 {
     /* Initialiser la position de la boule avec les coordonnées fournies */
     ball->rect.w = size;
@@ -153,6 +193,8 @@ void createBall(Ball *ball, int size, Vect position, Vect speed, SDL_Texture *te
     ball->rect.y = floor(position.y);
     ball->texture = texture;
     ball->position = position;
+    ball->mass = mass;
+    ball->colliding = -1;
 
     /* Initialiser la vitesse de la boule avec des valeurs aléatoires */
     ball->speed = speed;
@@ -161,8 +203,8 @@ void createBall(Ball *ball, int size, Vect position, Vect speed, SDL_Texture *te
 Vect posCenter(Ball *ball)
 {
     Vect res;
-    res.x = ball->rect.x + ball->rect.w / 2;
-    res.y = ball->rect.y + ball->rect.h / 2;
+    res.x = ball->position.x + ball->rect.w / 2;
+    res.y = ball->position.y + ball->rect.h / 2;
     return res;
 }
 
@@ -174,43 +216,32 @@ double distance(Ball *ball1, Ball *ball2)
     return norm(delta);
 }
 
-void calculer_collision(Ball *b1, Ball *b2, double mA, double mB, Vect *vA_f, Vect *vB_f)
+void calculer_collision(Ball *b1, Ball *b2, Vect *vA_f, Vect *vB_f)
 {
     // Calcul de la différence de position et de vitesse
     Vect delta_v = {b1->speed.x - b2->speed.x, b1->speed.y - b2->speed.y};
-    Vect delta_p = {b1->position.x - b2->position.x, b1->position.y - b2->position.y};
+    Vect p1 = posCenter(b1);
+    Vect p2 = posCenter(b2);
+    Vect delta_p = {p1.x - p2.x, p1.y - p2.y};
 
-    double alpha1 = atan2(delta_p.y, delta_p.x);
-    double beta1 = atan2(b1->speed.y, b1->speed.x);
-    double gamma1 = beta1 - alpha1;
+    double d = norm(delta_p);
 
-    double alpha2 = atan2(-delta_p.y, -delta_p.x);
-    double beta2 = atan2(b2->speed.y, b2->speed.x);
-    double gamma2 = beta2 - alpha2;
+    delta_p.x /= d;
+    delta_p.y /= d;
 
-    double v1 = norm(b1->speed);
-    double v2 = norm(b2->speed);
+    double p = 2*(dot(b1->speed, delta_p) - dot(b2->speed, delta_p)) / (b1->mass + b2->mass); 
 
-    double v12 = v1 * cos(gamma1);
-    double v21 = v2 * cos(gamma2);
-    double v11 = v1 * sin(gamma1);
-    double v22 = v2 * sin(gamma2);
+    vA_f->x = b1->speed.x - p * b2->mass * delta_p.x;
+    vA_f->y = b1->speed.y - p * b2->mass * delta_p.y;
 
-    double v1_f = (v12 * (mA - mB) - 2 * mB * v21) / (mA + mB);
-    double v2_f = (v21 * (mA - mB) + 2 * mA * v12) / (mA + mB);
-
-    vA_f->x = v1_f * cos(alpha1) - v11 * sin(alpha1);
-    vA_f->y = v1_f * sin(alpha1) + v11 * cos(alpha1);
-
-    vB_f->x = v2_f * cos(alpha2) - v22 * sin(alpha2);
-    vB_f->y = v2_f * sin(alpha2) + v22 * cos(alpha2);
+    vB_f->x = b2->speed.x + p * b1->mass * delta_p.x;
+    vB_f->y = b2->speed.y + p * b1->mass * delta_p.y;
+    
 }
 
-void updateBall(Ball *ball, Ball *balls, int num_boule_actuelle, int nb_balls)
+void updateBall(Uint64 dt, Ball *ball, Ball *balls, int num_boule_actuelle, int nb_balls)
 {
     int diametre = ball->rect.w;
-    ball->position.x += ball->speed.x;
-    ball->position.y += ball->speed.y;
 
     ball->rect.x = floor(ball->position.x);
     ball->rect.y = floor(ball->position.y);
@@ -218,34 +249,42 @@ void updateBall(Ball *ball, Ball *balls, int num_boule_actuelle, int nb_balls)
     if ((ball->rect.x <= 0 && ball->speed.x < 0) || (ball->rect.x + ball->rect.w >= SCREEN_WIDTH && ball->speed.x > 0))
     {
         ball->speed.x *= -1;
-        ball->position.x += ball->speed.x;
     }
     if ((ball->rect.y <= 0 && ball->speed.y < 0) || (ball->rect.y + ball->rect.h >= SCREEN_HEIGHT && ball->speed.y > 0))
     {
         ball->speed.y *= -1;
-        ball->position.y += ball->speed.y;
-    }
-    if (ball->rect.y + ball->rect.h >= SCREEN_HEIGHT)
-    {
-        ball->speed.y = -ball->speed.y;
-        ball->rect.y -= 5;
     }
     
 
     for (int i = num_boule_actuelle + 1; i < nb_balls; i++)
     {
         Ball *other = &balls[i];
+
+        double collideDistance = ((double) ball->rect.w + (double) other->rect.w) / 2;
+
         double dist = distance(ball, other);
 
-        if (dist <= diametre)
+        if (dist <= collideDistance && (ball->colliding == -1 && other->colliding == -1))
         {
             Vect vA_f;
             Vect vB_f;
-            calculer_collision(ball, other, 1, 1, &vA_f, &vB_f);
+            calculer_collision(ball, other, &vA_f, &vB_f);
             ball->speed = vA_f;
             other->speed = vB_f;
+
+            ball->colliding = i;
+            other->colliding = num_boule_actuelle;
+            printf("Collision entre %d et %d\n", num_boule_actuelle, i);
+        }
+        
+        if (dist > collideDistance && (ball->colliding == i && other->colliding == num_boule_actuelle)){
+            ball->colliding = -1;
+            other->colliding = -1;
         }
     }
+
+    ball->position.x += ball->speed.x * dt / 1000;
+    ball->position.y += ball->speed.y * dt / 1000;
 }
 
 void drawBall(SDL_Renderer *renderer, Ball *ball)
