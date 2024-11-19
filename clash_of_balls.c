1b1a9154630c3414fe624fb9622e9282bc08036b
Adam <adamnhl@MacBook-Air-de-Adam.local>
2024-11-19 11:02:47 +0100

    touche ESCAPE utilisable

--- clash_of_balls.c
+++ clash_of_balls.c
@@ -121,7 +121,7 @@ int main(int argc, char *argv[])
     {
         while (SDL_PollEvent(&event))
         {
-            if (event.type == SDL_QUIT)
+            if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
             {
                 isRunning = false;
             }
